eb9883bc72d479335bd30d250eee983d75abfb6d
Eliud2005 <utti232008@utvco.edu.mx>
2025-06-06 14:10:11 -0600

    Navbar y footer listos

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
-import { IonHeader, IonTitle,IonMenuButton, IonItem,IonMenu, IonList, IonContent, IonToolbar, IonButtons, IonImg } from "@ionic/angular/standalone";
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { IonHeader, IonTitle,IonMenuButton,IonRouterLink, IonItem,IonMenu, IonList, IonContent, IonToolbar, IonButtons, IonImg,} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  imports: [IonImg, IonButtons, IonToolbar, IonContent,IonMenuButton,IonMenu, IonList, IonItem, IonTitle, IonHeader,CommonModule],
+  imports: [  IonImg, IonButtons, IonToolbar,RouterLink,IonRouterLink, RouterLinkActive, IonContent,IonMenuButton,IonMenu, IonList, IonItem, IonTitle, IonHeader,CommonModule],
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent  implements OnInit {
 @Input({required: true}) title!: string;
+
+  logoFunction(){
+      window.location.href = "/home";
+  }
   constructor() { }
 
   ngOnInit() {}
