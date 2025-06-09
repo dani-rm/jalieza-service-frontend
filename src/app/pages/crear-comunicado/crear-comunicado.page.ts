0938be8474b5d8a613ba425b19c34c508b3f8568
Eliud2005 <utti232008@utvco.edu.mx>
2025-06-09 17:29:53 -0600

    Menu listo

--- src/app/pages/crear-comunicado/crear-comunicado.page.ts
+++ src/app/pages/crear-comunicado/crear-comunicado.page.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { NavbarComponent } from './../../components/navbar/navbar.component'
 import { FooterComponent } from './../../components/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-crear-comunicado',
