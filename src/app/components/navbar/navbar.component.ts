019295d3e905ad9061856a868520a620b16d4e10
MDZ2999 <mundoalexis1010@gmail.com>
2025-06-10 22:26:26 -0600

    Arreglar los inputs del formulario

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,22 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router } from '@angular/router';
 import { IonHeader, IonTitle,IonMenuButton,IonRouterLink, IonItem,IonMenu, IonList, IonContent, IonToolbar, IonButtons, IonImg, IonItemDivider, IonLabel } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  imports: [IonLabel,   IonImg, IonButtons, IonToolbar,RouterLink,IonRouterLink, RouterLinkActive, IonContent,IonMenuButton,IonMenu, IonList, IonItem, IonTitle, IonHeader,CommonModule],
+  imports: [IonLabel,   IonImg, IonButtons, IonToolbar, IonContent,IonMenuButton,IonMenu, IonList, IonItem, IonTitle, IonHeader,CommonModule],
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent  implements OnInit {
-@Input({required: true}) title!: string;
+  @Input({required: true}) title!: string;
+  constructor(public router: Router) {}
+
+  // Método para verificar si está en la página de "Registrar Ciudadano"
+  isRegistrarCiudadano(): boolean {
+    return this.router.url === '/registrar-ciudadano';
+  }
 
   logoFunction(){
       window.location.href = "/home";
   }
-  constructor() { }
-
   ngOnInit() {}
 
 }
--- src/app/pages/registrar-ciudadano/registrar-ciudadano.page.ts
+++ src/app/pages/registrar-ciudadano/registrar-ciudadano.page.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonLabel, IonButton, IonFooter, IonItem, IonInput, IonSelect, IonSelectOption, IonList, IonHeader, IonTitle, IonToolbar, IonRow, IonCol, IonGrid, IonButtons } from '@ionic/angular/standalone';
 import { NavbarComponent } from './../../components/navbar/navbar.component'
 import { FooterComponent } from './../../components/footer/footer.component';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registrar-ciudadano',
   templateUrl: './registrar-ciudadano.page.html',
   styleUrls: ['./registrar-ciudadano.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, NavbarComponent, FooterComponent]
+  imports: [IonButtons, IonGrid, IonCol, IonRow, IonToolbar, IonTitle, IonHeader, IonList, IonInput, IonItem, IonFooter, IonButton, IonLabel, IonContent, CommonModule,
+    FormsModule, NavbarComponent, FooterComponent, IonSelect, IonSelectOption]
 })
 export class RegistrarCiudadanoPage implements OnInit {
 
