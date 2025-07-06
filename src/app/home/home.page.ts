1f5008f1ac49d73ba659cfe7c477665977eaebac
Eliud2005 <utti232008@utvco.edu.mx>
2025-07-06 13:45:15 -0600

    Ya hace un login exitoso pero aun falta incorporar los guards para validar el token

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { addIcons } from 'ionicons';
 import { eye, eyeOff } from 'ionicons/icons';
+import { HttpClient } from '@angular/common/http';
+
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -15,15 +18,45 @@ import { eye, eyeOff } from 'ionicons/icons';
     NavbarComponent, FooterComponent, FormsModule],
 })
 export class HomePage {
-  constructor(public router: Router) {
+  email: string = '';
+  password: string = '';
+  showPassword: boolean = false;
+  constructor(public router: Router,
+      private http: HttpClient,
+    private alertController: AlertController,
+  ) {
     addIcons({eye, eyeOff});
+
   }
 
-  showPassword = false;
-  password = '';
+  /*showPassword = false;
+  password = '';*/
 
   togglePassword() {
     this.showPassword = !this.showPassword;
+  }
+    login() {
+    const body = {
+      email: this.email,
+      password: this.password,
+    };
+
+    this.http.post('http://localhost:3000/api/v1/auth/login', body).subscribe({
+      next: (response: any) => {
+        console.log('Login exitoso:', response);
+        localStorage.setItem('token', response.token); // Guarda token si quieres
+        // Puedes redirigir o mostrar mensaje
+      },
+      error: async (err) => {
+        console.error('Error en login', err);
+        const alert = await this.alertController.create({
+          header: 'Error',
+          message: err?.error?.message || 'No se pudo iniciar sesión',
+          buttons: ['OK'],
+        });
+        await alert.present();
+      },
+    });
   }
 
 }
